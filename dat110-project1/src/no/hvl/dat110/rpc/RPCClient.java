bf5a1f35e7e15fb1cc70e7d1ba14a97fa6afcbf8
h571544 <fredrik.1997@hotmail.com>
2019-02-01 12:52:35 +0100

    RPCClient done

--- dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
@@ -45,21 +45,21 @@ public class RPCClient {
 	}
 	
 	public byte[] call(byte[] rpcrequest) {
-		Message decapsulate = new Message(rpcrequest);
-		connection.send(decapsulate);
-		Message encapsulate = connection.receive();
-		
-		byte[] rpcreply= encapsulate.getData();
-		
 		/* 
-		
 		Make a remote call on the RPC server by sending a request message
 		and receive a reply message
 		
 		rpcrequest is the marshalled rpcrequest from the client-stub
 		rpctreply is the rpcreply to be unmarshalled by the client-stub
 		
-		*/
+		 */
+		
+		Message message = new Message(rpcrequest);
+		connection.send(message);
+		
+		Message received = connection.receive();
+		byte[] rpcreply = received.getData();
+		
 		
 		return rpcreply;
 		
